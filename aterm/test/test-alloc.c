4d5898e1a6ad56aeb789cc3a59c6f22953cafa63
Hayco de Jong <jong@metamud.org>
1998-04-15 10:38:55 +0000

    -Changed functionnames

--- aterm/test/test-alloc.c
+++ aterm/test/test-alloc.c
@@ -11,11 +11,11 @@ int main(int argc, char *argv[])
   int i, bottomOfStack;
   ATerm *test;
 
-  Tinit(argc, argv, NULL, &bottomOfStack);
+  ATinit(argc, argv, NULL, &bottomOfStack);
 
   fprintf(stderr, "Allocating 18 nodes of size 3:\n");
   for(i=0; i<18; i++) {
-    test = T_allocate(3);
+    test = AT_allocate(3);
     if(test)
       fprintf(stderr, "Result: %p\n", test);
     else
