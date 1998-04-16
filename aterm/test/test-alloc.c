7a5a4b47bcc61526dbafee65b3afda767954f07e
Hayco de Jong <jong@metamud.org>
1998-04-16 13:14:50 +0000

    - Added symbol-testing

--- aterm/test/test-alloc.c
+++ aterm/test/test-alloc.c
@@ -3,15 +3,16 @@
   * Test the node allocation functions
   */
 
-#include "memory.h"
 #include <stdio.h>
+#include <assert.h>
+#include "memory.h"
+#include "symbol.h"
 
-int main(int argc, char *argv[])
+void
+testAlloc(void)
 {
-  int i, bottomOfStack;
+  int i;
   ATerm *test;
-
-  ATinit(argc, argv, NULL, &bottomOfStack);
 
   fprintf(stderr, "Allocating 18 nodes of size 3:\n");
   for(i=0; i<18; i++) {
@@ -23,6 +24,39 @@ int main(int argc, char *argv[])
   }
 
   printf("test succeeded.\n");
+}
+
+void
+testSymbol(void)
+{
+	int i;
+	Symbol *symmies[16];
+
+	symmies[0] = ATmakeSymbol("aanroep", 3, ATfalse);
+	symmies[1] = ATmakeSymbol("aanroep", 3, ATtrue);
+	symmies[2] = ATmakeSymbol("aanroep", 4, ATfalse);
+	symmies[3] = ATmakeSymbol("aanroep", 4, ATtrue);
+	symmies[4] = ATmakeSymbol("aanroep", 3, ATfalse);
+
+	assert(symmies[0] == symmies[4]);
+	for (i=0; i<5; i++)
+	{
+		fprintf(stdout, "symmies[%d]: ", i);
+		AT_printSymbol(symmies[i], stdout);
+	}
+	assert(symmies[1] != symmies[2]);
+	assert(symmies[1] != symmies[3]);
+	assert(symmies[2] != symmies[3]);
+}
+
+int main(int argc, char *argv[])
+{
+  int bottomOfStack;
+
+  ATinit(argc, argv, NULL, &bottomOfStack);
+
+  testAlloc();
+  testSymbol();
 
   return 0;
 }
