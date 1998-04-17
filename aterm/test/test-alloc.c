54d8c6c6d408837f164f015e87444a314b4a7de6
Pieter Olivier <pieter@gamesquare.nl>
1998-04-17 13:11:39 +0000

    -updated testcases to reflect changes in source

--- aterm/test/test-alloc.c
+++ aterm/test/test-alloc.c
@@ -12,7 +12,7 @@ void
 testAlloc(void)
 {
   int i;
-  ATerm *test;
+  ATerm test;
 
   fprintf(stderr, "Allocating 18 nodes of size 3:\n");
   for(i=0; i<18; i++) {
@@ -30,23 +30,55 @@ void
 testSymbol(void)
 {
 	int i;
-	Symbol *symmies[16];
+	Symbol symmies[8];
 
-	symmies[0] = ATmakeSymbol("aanroep", 3, ATfalse);
-	symmies[1] = ATmakeSymbol("aanroep", 3, ATtrue);
-	symmies[2] = ATmakeSymbol("aanroep", 4, ATfalse);
-	symmies[3] = ATmakeSymbol("aanroep", 4, ATtrue);
-	symmies[4] = ATmakeSymbol("aanroep", 3, ATfalse);
+	symmies[0] = ATmakeSymbol("application", 3, ATfalse);
+	symmies[1] = ATmakeSymbol("application", 3, ATtrue);
+	symmies[2] = ATmakeSymbol("application", 4, ATfalse);
+	symmies[3] = ATmakeSymbol("application", 4, ATtrue);
+	symmies[4] = ATmakeSymbol("application", 3, ATfalse);
 
 	assert(symmies[0] == symmies[4]);
 	for (i=0; i<5; i++)
 	{
 		fprintf(stdout, "symmies[%d]: ", i);
 		AT_printSymbol(symmies[i], stdout);
+		fprintf(stdout, "\n");
 	}
 	assert(symmies[1] != symmies[2]);
 	assert(symmies[1] != symmies[3]);
 	assert(symmies[2] != symmies[3]);
+}
+
+void
+testAppl(void)
+{
+  int i;
+  Symbol symmies[4];
+  ATermAppl apples[16];
+
+  symmies[0] = ATmakeSymbol("f0", 0, ATfalse);
+  symmies[1] = ATmakeSymbol("f1", 1, ATfalse);
+  symmies[2] = ATmakeSymbol("f6", 6, ATfalse);
+
+  apples[0] = ATmakeAppl0(symmies[0]);
+  apples[1] = ATmakeAppl1(symmies[1], (ATerm)apples[0]);
+  apples[2] = ATmakeAppl1(symmies[1], (ATerm)apples[1]);
+  apples[3] = ATmakeAppl1(symmies[1], (ATerm)apples[0]);
+  apples[4] = ATmakeAppl6(symmies[2], (ATerm)apples[0], (ATerm)apples[0], 
+			  (ATerm)apples[1], (ATerm)apples[0], 
+			  (ATerm)apples[0], (ATerm)apples[1]);
+
+  assert(apples[1] == apples[3]);
+  assert(apples[1] != apples[2]);
+  assert(apples[2] != apples[3]);
+  assert(apples[0] != apples[1]);
+
+  for(i=0; i<5; i++) {
+    fprintf(stdout, "apples[%d] = ", i);
+    ATwriteToTextFile((ATerm)apples[i], stdout);
+    fprintf(stdout, "\n");
+  }
 }
 
 int main(int argc, char *argv[])
@@ -57,6 +89,7 @@ int main(int argc, char *argv[])
 
   testAlloc();
   testSymbol();
+  testAppl();
 
   return 0;
 }
