ce922f42c391f803dcf543d8748de7d3d41955a4
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
1998-04-28 15:57:54 +0000

    asfixtest.c adapted so that it reads an asfix file and expands it to asfix.

--- aterm/test/asfixtest.c
+++ aterm/test/asfixtest.c
@@ -1,35 +1,35 @@
 #include <expand2asfix.h>
+#include <init-patterns.h>
 #include <asfix.h>
 
 int main(int argc, char **argv)
 {
   char *fname;
-/*
   ATerm mod, expmod;
   FILE *output, *input;
-*/
 
   int bottomOfStack;
 
-  fname = "/home/markvdb/NEW-META/aterm/asfix/tests/Test.asfix";
+  fname = "/home/markvdb/NEW-META/aterm/test/Test.asfix";
 
   ATinit(argc,argv, NULL, &bottomOfStack);
 
   AFinit();
   AFinitExpansionTerms();
-/*
+  AFinitAsFixPatterns();
+
   input = fopen(fname,"r");
   if(input) {
     mod = ATreadFromTextFile(input);
     fclose(input);
   }
-  fname = "/home/markvdb/NEW-META/aterm/asfix/tests/Test.asfix.asfix";
+  fname = "/home/markvdb/NEW-META/aterm/test/Test.asfix.asfix";
   expmod = AFexpandModuleToAsFix(mod,fname);
-  ATprintf("Writing: %s.asfix\n", fname);
+  ATprintf("Writing: %s\n", fname);
   output = fopen(fname,"w");
   ATwriteToTextFile(expmod, output);
   fclose(output);
-*/
+
   ATprintf("AsFixTest was successful\n");
 
   return 0;
