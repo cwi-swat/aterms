d77efbbd92fc35737ce3929bf592276796877c61
Hayco de Jong <jong@metamud.org>
1998-06-26 13:10:22 +0000

    - Added #ifdef guards around MIN and MAX to avoid redefinition warnings   when compiling on platforms that define MIN/MAX in a system include.

--- aterm/aterm/util.h
+++ aterm/aterm/util.h
@@ -4,8 +4,14 @@
 #include <string.h>
 
 #define streq(s,t)	(!(strcmp(s,t)))
-#define MIN(a,b)    ((a)<(b) ? (a) : (b))
-#define MAX(a,b)    ((a)>(b) ? (a) : (b))
+
+#ifndef MIN
+#  define MIN(a,b)    ((a)<(b) ? (a) : (b))
+#endif
+
+#ifndef MAX
+#  define MAX(a,b)    ((a)>(b) ? (a) : (b))
+#endif
 
 #define IDX_TOTAL               0
 #define IDX_MIN                 1
