c9bedace1a057626e95dbcb1f8a86daeb53aabf0
Hayco de Jong <jong@metamud.org>
1998-08-19 12:26:03 +0000

    - match.c is now a compilable example, rather than just exemplary statements.

--- aterm/doc/examples/match.c
+++ aterm/doc/examples/match.c
@@ -1,15 +1,32 @@
-ATbool result;
-ATerm  list;
-int    ival;
+#include <aterm2.h>
 
-/* Sets result to ATtrue and rval to 16. */
-result = ATmatch(ATmake("f(16)"), "f(<int>)", &ival);
+void foo()
+{
+    ATbool result;
+    ATerm  list;
+    double rval;
+    int    ival;
 
-/* Sets result to ATtrue and rval to 3.14. */
-result = ATmatch(ATmake("3.14"), "<real>", &rval);
+    /* Sets result to ATtrue and rval to 16. */
+    result = ATmatch(ATmake("f(16)"), "f(<int>)", &ival);
 
-/* Sets result to ATfalse because f(g) != g(f) */
-result = ATmatch(ATmake("f(g)"), "g(f)");
+    /* Sets result to ATtrue and rval to 3.14. */
+    result = ATmatch(ATmake("3.14"), "<real>", &rval);
 
-/* fills ival with 1 and list with [2,3] */
-result = ATmatch(ATmake("[1,2,3]"), "[<int>,<list>]", &ival, &list);
+    /* Sets result to ATfalse because f(g) != g(f) */
+    result = ATmatch(ATmake("f(g)"), "g(f)");
+
+    /* fills ival with 1 and list with [2,3] */
+    result = ATmatch(ATmake("[1,2,3]"), "[<int>,<list>]", &ival, &list);
+}
+
+int main(int argc, char *argv[])
+{
+    ATerm bottomOfStack;
+
+    ATinit(argc, argv, &bottomOfStack);
+
+    foo();
+    
+    return 0;
+}
