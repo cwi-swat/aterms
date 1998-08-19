74aa73c1106eb45355c51d4753bd5099ae49ce74
Hayco de Jong <jong@metamud.org>
1998-08-19 08:58:30 +0000

    - Elaborated on pattern <list> when matching [1,2,3,4] with [1,<list>]. - make.c is now a compilable example

--- aterm/doc/examples/match.c
+++ aterm/doc/examples/match.c
@@ -1,4 +1,6 @@
 ATbool result;
+ATerm  list;
+int    ival;
 
 /* Sets result to ATtrue and rval to 16. */
 result = ATmatch(ATmake("f(16)"), "f(<int>)", &ival);
@@ -8,3 +10,6 @@ result = ATmatch(ATmake("3.14"), "<real>", &rval);
 
 /* Sets result to ATfalse because f(g) != g(f) */
 result = ATmatch(ATmake("f(g)"), "g(f)");
+
+/* fills ival with 1 and list with [2,3] */
+result = ATmatch(ATmake("[1,2,3]"), "[<int>,<list>]", &ival, &list);
