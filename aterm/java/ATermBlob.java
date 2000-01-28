a106c125d2dd97734b1f734708f62b5d40c5f168
Hayco de Jong <jong@metamud.org>
2000-01-28 12:22:19 +0000

    - Added ATermBlob interface

---
+++ aterm/java/ATermBlob.java
@@ -0,0 +1,27 @@
+package aterm;
+
+/**
+ * This interface describes the functionality of an ATermBlob
+ * (Binary Large OBject).
+ * 
+ * @author Hayco de Jong (jong@cwi.nl)
+ * @author Pieter Olivier (olivierp@cwi.nl)
+ * @version 0.1, Fri Jan 28 10:19:58 MET 2000
+ */
+public interface ATermBlob extends ATerm {
+
+    /**
+     * Gets the size (in bytes) of the data in this blob.
+     *
+     * @param the size of the data in this blob.
+     */
+    public int getSize();
+
+    /**
+     * Gets the data in this blob.
+     *
+     * @return the data in this blob.
+     *
+     */
+    public byte[] getData();
+}
