c8d9438daaab4ed434ae1123ed99e40915a9237e
Pieter Olivier <pieter@gamesquare.nl>
2000-03-10 15:22:59 +0000

    Added -h support

--- aterm/aterm/termsize.c
+++ aterm/aterm/termsize.c
@@ -5,21 +5,31 @@
 
 void termsize(FILE *file)
 {
-	int core_size, text_size, term_depth;
-
-	ATerm t = ATreadFromFile(file);
-	core_size = AT_calcCoreSize(t);
-	text_size = AT_calcTextSize(t);
-	term_depth = AT_calcTermDepth(t);
-	printf("internal size: %d bytes, text size: %d bytes, depth: %d\n", 
-				 core_size, text_size, term_depth);
+  int core_size, text_size, term_depth;
+  
+  ATerm t = ATreadFromFile(file);
+  core_size = AT_calcCoreSize(t);
+  text_size = AT_calcTextSize(t);
+  term_depth = AT_calcTermDepth(t);
+  printf("internal size: %d bytes, text size: %d bytes, depth: %d\n", 
+	 core_size, text_size, term_depth);
 }
 
 int main(int argc, char *argv[])
 {
+  int i;
   ATerm bottomOfStack;
-	ATinit(argc, argv, &bottomOfStack);
-	termsize(stdin);
 
-	return 0;
+  for(i=1; i<argc; i++) {
+    if(strcmp(argv[i], "-h") == 0) {
+      ATwarning("usage: %s [aterm-options] < input > output\n", argv[0]);
+      ATwarning("  use -at-help to get aterm-options\n");
+      exit(0);
+    }
+  }
+
+  ATinit(argc, argv, &bottomOfStack);
+  termsize(stdin);
+  
+  return 0;
 }
