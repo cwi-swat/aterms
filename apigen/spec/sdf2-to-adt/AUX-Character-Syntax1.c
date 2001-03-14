1419f0cfdb5d7b8a24e479d71cd00352f27ddcc5
Mark van den Brand <M.G.J.v.d.Brand@tue.nl>
2001-03-14 08:46:35 +0000

    - Added new files. - Generated new code.

--- apigen/spec/sdf2-to-adt/AUX-Character-Syntax1.c
+++ apigen/spec/sdf2-to-adt/AUX-Character-Syntax1.c
@@ -12,9 +12,13 @@ static ATerm lf_AUX_Character_Syntax1_4 ( ) ;
 static Symbol lf_AUX_Character_Syntax1_5sym ;
 static ATerm lf_AUX_Character_Syntax1_5 ( ) ;
 static Symbol lf_AUX_Character_Syntax1_6sym ;
-static ATerm lf_AUX_Character_Syntax1_6 ( ATerm arg1 ) ;
+static ATerm lf_AUX_Character_Syntax1_6 ( ) ;
 static Symbol lf_AUX_Character_Syntax1_7sym ;
-static ATerm lf_AUX_Character_Syntax1_7 ( ATerm arg1 ) ;
+static ATerm lf_AUX_Character_Syntax1_7 ( ) ;
+static Symbol lf_AUX_Character_Syntax1_8sym ;
+static ATerm lf_AUX_Character_Syntax1_8 ( ATerm arg1 ) ;
+static Symbol lf_AUX_Character_Syntax1_9sym ;
+static ATerm lf_AUX_Character_Syntax1_9 ( ATerm arg1 ) ;
 void register_AUX_Character_Syntax1 ( ) {
 lf_AUX_Character_Syntax1_2sym = ATmakeSymbol ( "prod(id(\"GEN-LexConsFuncs\"),w(\"\"),[ql(\"shortchar\"),w(\"\"),ql(\"(\"),w(\"\"),iter(sort(\"CHAR\"),w(\"\"),l(\"*\")),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ShortChar\"),w(\"\"),no-attrs)"
  , 1 , ATtrue ) ;
@@ -23,33 +27,47 @@ lf_AUX_Character_Syntax1_1sym = ATmakeSymbol ( "listtype(sort(\"CHAR\"))" , 1 , 
 ATprotectSymbol ( lf_AUX_Character_Syntax1_1sym ) ;
 lf_AUX_Character_Syntax1_3sym = ATmakeSymbol ( "prod(id(\"GEN-LexConsFuncs\"),w(\"\"),[ql(\"numchar\"),w(\"\"),ql(\"(\"),w(\"\"),iter(sort(\"CHAR\"),w(\"\"),l(\"*\")),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"NumChar\"),w(\"\"),no-attrs)" , 1 , ATtrue ) ;
 ATprotectSymbol ( lf_AUX_Character_Syntax1_3sym ) ;
-lf_AUX_Character_Syntax1_4sym = ATmakeSymbol ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\EOF\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" , 0 , ATtrue ) ;
+lf_AUX_Character_Syntax1_4sym = ATmakeSymbol ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\LABEL_START\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" , 0 , ATtrue ) ;
 ATprotectSymbol ( lf_AUX_Character_Syntax1_4sym ) ;
-lf_AUX_Character_Syntax1_5sym = ATmakeSymbol ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\TOP\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" , 0 , ATtrue ) ;
+lf_AUX_Character_Syntax1_5sym = ATmakeSymbol ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\BOT\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" , 0 , ATtrue ) ;
 ATprotectSymbol ( lf_AUX_Character_Syntax1_5sym ) ;
-lf_AUX_Character_Syntax1_6sym = ATmakeSymbol ( "prod(id(\"Character-Syntax\"),w(\"\"),[sort(\"ShortChar\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" , 1 , ATtrue ) ;
+lf_AUX_Character_Syntax1_6sym = ATmakeSymbol ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\EOF\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" , 0 , ATtrue ) ;
 ATprotectSymbol ( lf_AUX_Character_Syntax1_6sym ) ;
-lf_AUX_Character_Syntax1_7sym = ATmakeSymbol ( "prod(id(\"Character-Syntax\"),w(\"\"),[sort(\"NumChar\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" , 1 , ATtrue ) ;
+lf_AUX_Character_Syntax1_7sym = ATmakeSymbol ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\TOP\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" , 0 , ATtrue ) ;
 ATprotectSymbol ( lf_AUX_Character_Syntax1_7sym ) ;
+lf_AUX_Character_Syntax1_8sym = ATmakeSymbol ( "prod(id(\"Character-Syntax\"),w(\"\"),[sort(\"ShortChar\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" , 1 , ATtrue ) ;
+ATprotectSymbol ( lf_AUX_Character_Syntax1_8sym ) ;
+lf_AUX_Character_Syntax1_9sym = ATmakeSymbol ( "prod(id(\"Character-Syntax\"),w(\"\"),[sort(\"NumChar\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" , 1 , ATtrue ) ;
+ATprotectSymbol ( lf_AUX_Character_Syntax1_9sym ) ;
 register_prod ( ATparse ( "listtype(sort(\"CHAR\"))" ) , lf_AUX_Character_Syntax1_1 , lf_AUX_Character_Syntax1_1sym ) ;
 register_prod ( ATparse ( "prod(id(\"GEN-LexConsFuncs\"),w(\"\"),[ql(\"shortchar\"),w(\"\"),ql(\"(\"),w(\"\"),iter(sort(\"CHAR\"),w(\"\"),l(\"*\")),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"ShortChar\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_2 , lf_AUX_Character_Syntax1_2sym ) ;
 register_prod ( ATparse ( "prod(id(\"GEN-LexConsFuncs\"),w(\"\"),[ql(\"numchar\"),w(\"\"),ql(\"(\"),w(\"\"),iter(sort(\"CHAR\"),w(\"\"),l(\"*\")),w(\"\"),ql(\")\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"NumChar\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_3 , lf_AUX_Character_Syntax1_3sym ) ;
-register_prod ( ATparse ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\EOF\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_4 , lf_AUX_Character_Syntax1_4sym ) ;
-register_prod ( ATparse ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\TOP\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_5 , lf_AUX_Character_Syntax1_5sym ) ;
-register_prod ( ATparse ( "prod(id(\"Character-Syntax\"),w(\"\"),[sort(\"ShortChar\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_6 , lf_AUX_Character_Syntax1_6sym ) ;
-register_prod ( ATparse ( "prod(id(\"Character-Syntax\"),w(\"\"),[sort(\"NumChar\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_7 , lf_AUX_Character_Syntax1_7sym ) ;
+register_prod ( ATparse ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\LABEL_START\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_4 , lf_AUX_Character_Syntax1_4sym ) ;
+register_prod ( ATparse ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\BOT\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_5 , lf_AUX_Character_Syntax1_5sym ) ;
+register_prod ( ATparse ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\EOF\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_6 , lf_AUX_Character_Syntax1_6sym ) ;
+register_prod ( ATparse ( "prod(id(\"Character-Syntax\"),w(\"\"),[ql(\"\\\\\\\\TOP\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_7 , lf_AUX_Character_Syntax1_7sym ) ;
+register_prod ( ATparse ( "prod(id(\"Character-Syntax\"),w(\"\"),[sort(\"ShortChar\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_8 , lf_AUX_Character_Syntax1_8sym ) ;
+register_prod ( ATparse ( "prod(id(\"Character-Syntax\"),w(\"\"),[sort(\"NumChar\")],w(\"\"),l(\"->\"),w(\"\"),sort(\"Character\"),w(\"\"),no-attrs)" ) , lf_AUX_Character_Syntax1_9 , lf_AUX_Character_Syntax1_9sym ) ;
 }
 void resolve_AUX_Character_Syntax1 ( ) {
 }
 void init_AUX_Character_Syntax1 ( ) {
 }
-ATerm lf_AUX_Character_Syntax1_7 ( ATerm arg0 ) {
-CONS_ENTRY ( lf_AUX_Character_Syntax1_7sym , ATmakeAppl ( lf_AUX_Character_Syntax1_7sym , arg0 ) ) ;
-CONS_EXIT ( make_nf1 ( lf_AUX_Character_Syntax1_7sym , arg0 ) ) ;
+ATerm lf_AUX_Character_Syntax1_9 ( ATerm arg0 ) {
+CONS_ENTRY ( lf_AUX_Character_Syntax1_9sym , ATmakeAppl ( lf_AUX_Character_Syntax1_9sym , arg0 ) ) ;
+CONS_EXIT ( make_nf1 ( lf_AUX_Character_Syntax1_9sym , arg0 ) ) ;
 }
-ATerm lf_AUX_Character_Syntax1_6 ( ATerm arg0 ) {
-CONS_ENTRY ( lf_AUX_Character_Syntax1_6sym , ATmakeAppl ( lf_AUX_Character_Syntax1_6sym , arg0 ) ) ;
-CONS_EXIT ( make_nf1 ( lf_AUX_Character_Syntax1_6sym , arg0 ) ) ;
+ATerm lf_AUX_Character_Syntax1_8 ( ATerm arg0 ) {
+CONS_ENTRY ( lf_AUX_Character_Syntax1_8sym , ATmakeAppl ( lf_AUX_Character_Syntax1_8sym , arg0 ) ) ;
+CONS_EXIT ( make_nf1 ( lf_AUX_Character_Syntax1_8sym , arg0 ) ) ;
+}
+ATerm lf_AUX_Character_Syntax1_7 ( ) {
+CONS_ENTRY ( lf_AUX_Character_Syntax1_7sym , ATmakeAppl0 ( lf_AUX_Character_Syntax1_7sym ) ) ;
+CONS_EXIT ( make_nf0 ( lf_AUX_Character_Syntax1_7sym ) ) ;
+}
+ATerm lf_AUX_Character_Syntax1_6 ( ) {
+CONS_ENTRY ( lf_AUX_Character_Syntax1_6sym , ATmakeAppl0 ( lf_AUX_Character_Syntax1_6sym ) ) ;
+CONS_EXIT ( make_nf0 ( lf_AUX_Character_Syntax1_6sym ) ) ;
 }
 ATerm lf_AUX_Character_Syntax1_5 ( ) {
 CONS_ENTRY ( lf_AUX_Character_Syntax1_5sym , ATmakeAppl0 ( lf_AUX_Character_Syntax1_5sym ) ) ;
