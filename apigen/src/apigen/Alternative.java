d1353d06d223af2ecf2f9ba57e6f9d3bc6e1f71e
Pieter Olivier <pieter@gamesquare.nl>
2001-03-30 14:15:20 +0000

    Added support for native types (int, real, str)

--- apigen/src/apigen/Alternative.java
+++ apigen/src/apigen/Alternative.java
@@ -4,6 +4,8 @@ import aterm.*;
 
 public class Alternative
 {
+  private final static String[] RESERVED_TYPES = { "int", "real", "str" };
+
   String id;
   ATerm  pattern;
 
@@ -29,6 +31,80 @@ public class Alternative
   public ATerm getPattern()
   {
     return pattern;
+  }
+
+  //}}}
+  //{{{ public ATerm buildMatchPattern()
+
+  public ATerm buildMatchPattern()
+  {
+    return buildMatchPattern(pattern);
+  }
+
+  //}}}
+  //{{{ private ATerm buildMatchPattern(ATerm t)
+
+  private ATerm buildMatchPattern(ATerm t)
+  {
+    switch (t.getType()) {
+      case ATerm.APPL:
+	{
+	  ATermAppl appl = (ATermAppl)t;
+	  AFun fun = appl.getAFun();
+	  ATerm[] newargs = new ATerm[fun.getArity()];
+	  for (int i=0; i<fun.getArity(); i++) {
+	    newargs[i] = buildMatchPattern(appl.getArgument(i));
+	  }
+	  return pattern.getFactory().makeAppl(fun, newargs);
+	}
+	
+      case ATerm.LIST:
+	{
+	  ATermList list = (ATermList)t;
+	  ATerm[] elems = new ATerm[list.getLength()];
+	  int i = 0;
+	  while (!list.isEmpty()) {
+	    elems[i++] = buildMatchPattern(list.getFirst());
+	    list = list.getNext();
+	  }
+	  for (i=elems.length-1; i>=0; i--) {
+	    list = list.insert(elems[i]);
+	  }
+	  return list;
+	}
+
+      case ATerm.PLACEHOLDER:
+	{
+	  ATerm ph = ((ATermPlaceholder)t).getPlaceholder();
+	  if (ph.getType() == ATerm.LIST) {
+	    return pattern.getFactory().parse("<list>");
+	  } else { 
+	    ATerm type = ((ATermAppl)ph).getArgument(0);
+	    if (isReservedType(type.toString())) {
+	      return pattern.getFactory().makePlaceholder(type);
+	    } else {
+	      return pattern.getFactory().parse("<term>");
+	    }
+	  }
+	}
+
+      default:
+	return t;
+    }
+  }
+
+  //}}}
+  //{{{ public boolean isReservedType(ATerm t)
+
+  static public boolean isReservedType(String type)
+  {
+    for (int i=0; i<RESERVED_TYPES.length; i++) {
+      if (RESERVED_TYPES[i].equals(type)) {
+	return true;
+      }
+    }
+
+    return false;
   }
 
   //}}}
