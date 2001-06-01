7591ac79ed77846a9838754a2c9310a4107c2aa4
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2001-06-01 15:20:21 +0000

    * made ADT-utils compliant with new conversion syntax

--- apigen/utils/ADT-utils.c
+++ apigen/utils/ADT-utils.c
@@ -25,21 +25,21 @@ ADTforeachEntry(ADTEntries entries, ADTEntryFunc func, void *user_data)
 ADTEntries
 ADTsubstitute(ADTEntries entries, ADTSubstFunc substitute, void *user_data)
 {
-  ATermList todo  = (ATermList)ADTmakeTermFromEntries(entries);
+  ATermList todo  = (ATermList)ADTEntriesToTerm(entries);
   ATermList done = ATempty;
 
   while (!ATisEmpty(todo)) {
-    ADTEntry before = ADTmakeEntryFromTerm(ATgetFirst(todo));
+    ADTEntry before = ADTEntryFromTerm(ATgetFirst(todo));
     ADTEntry after  = substitute(before, user_data);
 
     if (after != NULL) {
-      done = ATinsert(done, ADTmakeTermFromEntry(after));
+      done = ATinsert(done, ADTEntryToTerm(after));
     }
 
     todo = ATgetNext(todo);
   }
 
-  return ADTmakeEntriesFromTerm((ATerm)ATreverse(done));
+  return ADTEntriesFromTerm((ATerm)ATreverse(done));
 }
 
 /*}}}  */
