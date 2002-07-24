e78c55c7e0fabf4975ea10aeb6fc468330aed896
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2002-07-24 15:32:28 +0000

    * Made aterm2xml compliant with Meta-Environment commandline interface   * Made xml2aterm compliant with Meta-Environment commandline interface

--- aterm-xml/config/globals.h
+++ aterm-xml/config/globals.h
@@ -2,17 +2,17 @@
  * ( START WITH A LETTER AND CONTAIN ONLY ALPHANUM CHARS OR - OR _ )
  */
 /* the name to identify a quoted AFun */
-static char* quoted_ATermAppl_name = "AFun";
-static char* quoted_AFun_name = "fun";
+char* quoted_ATermAppl_name = "AFun";
+char* quoted_AFun_name = "fun";
 
 /* the name to identify an aterm list in XML */
-static char* ax_list_name = "ATermList";
+char* ax_list_name = "ATermList";
 
 /* the name to identify an aterm blob in XML */
-static char* ax_blob_name = "_a2x_blob_name_";
+char* ax_blob_name = "_a2x_blob_name_";
 
 /* the name to identify an aterm placeholder in XML */
-static char* ax_ph_name = "_a2x_ph_name_";
+char* ax_ph_name = "_a2x_ph_name_";
 
 /* WE ARE DONE ASSUMING HERE
  */
