3fcbde5470a3363ed0e2c199ab1b8efbb6c52e94
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-01-23 17:43:42 +0000

    * changed license

--- shared-objects/shared/HashFunctions.java
+++ shared-objects/shared/HashFunctions.java
@@ -1,5 +1,12 @@
-package shared;
+/* Copyright (c) 2003, CWI, LORIA-INRIA All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ */
 
+package shared;
 
 public class HashFunctions {
 	
