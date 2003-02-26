87827a3748cadffe14c695b0d9cca605b305e9df
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-02-26 16:49:27 +0000

    *** empty log message ***

--- apigen/src/apigen/gen/Generator.java
+++ apigen/src/apigen/gen/Generator.java
@@ -8,46 +8,37 @@ import java.io.PrintStream;
 public abstract class Generator {
 	// current stream to write to
 	protected PrintStream stream;
-	
-	protected StringConversions stringConverter;
-	protected TypeConverter      typeConverter;
 
+    // we always need string conversions and type conversions to
+    // map apigen strings to identifiers accepted in the generated language
+	protected StringConversions stringConverter;
+	protected TypeConverter typeConverter;
+	abstract protected void initTypeConverter();
+	
 	// basic generator options
 	protected static boolean verbose = false;
 	protected static boolean folding = false;
 
-    protected Generator() {
-    	initStringConverter();
-    	initTypeConverter();	
-    }
-	
+	protected Generator() {
+		initStringConverter();
+		initTypeConverter();
+	}
+
 	public void initStringConverter() {
 		stringConverter = new StringConversions();
 	}
-	
-	abstract protected void initTypeConverter();
-	
+
 	protected void println() {
 		stream.println();
 	}
-
-	//}}}
-	//{{{ protected void println(String msg)
 
 	protected void println(String msg) {
 		stream.println(msg);
 	}
 
-	//}}}
-	//{{{ protected void print(String msg)
-
 	protected void print(String msg) {
 		stream.print(msg);
 	}
-
-	//}}}
-
-	//{{{ protected void info(String msg)
 
 	protected void info(String msg) {
 		if (verbose) {
@@ -55,33 +46,32 @@ public abstract class Generator {
 		}
 	}
 
-	//}}}
-  
-  protected void createStream(String file) 
-  {
-    try {
-      stream = new PrintStream(new FileOutputStream(file));
-    }
-    catch (FileNotFoundException exc) {
-      System.err.println("fatal error: Failed to open " + file + " for writing.");
-      System.exit(1);
-    }
-  }
-  
-  protected void createFileStream(String name, String ext, String directory) {
-	  char sep = File.separatorChar;
-	  File base = new File(directory);
-    
-	  if (!base.exists()) {
-		if(!base.mkdirs()) {
-		  throw new RuntimeException("could not create output directory " + directory);
+	protected void createStream(String file) {
+		try {
+			stream = new PrintStream(new FileOutputStream(file));
+		} catch (FileNotFoundException exc) {
+			System.err.println(
+				"fatal error: Failed to open " + file + " for writing.");
+			System.exit(1);
 		}
-	  }
-	  else if (!base.isDirectory()) {
-		throw new RuntimeException(directory + " is not a directory");
-	  }
-    
-	  createStream(directory + sep + name + ext);
 	}
 
+	protected void createFileStream(
+		String name,
+		String ext,
+		String directory) {
+		char sep = File.separatorChar;
+		File base = new File(directory);
+
+		if (!base.exists()) {
+			if (!base.mkdirs()) {
+				throw new RuntimeException(
+					"could not create output directory " + directory);
+			}
+		} else if (!base.isDirectory()) {
+			throw new RuntimeException(directory + " is not a directory");
+		}
+
+		createStream(directory + sep + name + ext);
+	}
 }
