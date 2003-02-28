bca06c194681fb6254a8c6b404258c80241338a9
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-02-28 00:57:29 +0000

    Merged Pierre-Etienne changes with mine Finished refactoring of C backend (let's see if everything still works...)

---
+++ apigen/apigen/gen/c/CDictionaryGenerator.java
@@ -0,0 +1,94 @@
+package apigen.gen.c;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import apigen.adt.*;
+import apigen.gen.Generator;
+import apigen.gen.StringConversions;
+import aterm.*;
+import aterm.pure.PureFactory;
+
+public class CDictionaryGenerator extends Generator {
+	private ADT adt;
+	private String prefix;
+	private String apiName;
+
+	private ATermFactory factory;
+
+	private Map afuns_by_afun;
+	private int nextAFun;
+	private Map afuns_by_name;
+
+	public CDictionaryGenerator(
+		ADT adt,
+		String directory,
+		String apiName,
+		String prefix,
+		boolean verbose,
+		boolean folding) {
+		super(directory, apiName, ".dict", verbose, folding);
+		this.adt = adt;
+		this.apiName = apiName;
+		this.prefix = prefix;
+		this.factory = new PureFactory();
+		
+		afuns_by_name = new HashMap();
+		afuns_by_afun = new HashMap();
+	}
+
+	protected void generate() {
+		info("generating " + apiName + ".dict");
+
+		try {
+			buildDictionary(adt).writeToTextFile(stream);
+		} catch (IOException e) {
+			System.out.println("Could not write to dictionary file.");
+			System.exit(1);
+		}
+	}
+
+	private ATerm buildDictionary(ADT adt) {
+		ATermList afun_list = factory.makeList();
+		for (int i = nextAFun - 1; i >= 0; i--) {
+			String name = prefix + "afun" + i;
+			AFun afun = (AFun) afuns_by_name.get(name);
+			ATerm[] args = new ATerm[afun.getArity()];
+			for (int j = 0; j < afun.getArity(); j++) {
+				args[j] = factory.parse("x");
+			}
+			ATerm term = factory.makeAppl(afun, args);
+			afun_list =
+				afun_list.insert(factory.make("[" + name + ",<term>]", term));
+		}
+
+		ATermList term_list = factory.makeList();
+
+		Iterator types = adt.typeIterator();
+		while (types.hasNext()) {
+			Type type = (Type) types.next();
+			String id = StringConversions.makeIdentifier(type.getId());
+			Iterator alts = type.alternativeIterator();
+			while (alts.hasNext()) {
+				Alternative alt = (Alternative) alts.next();
+				ATerm entry =
+					factory.make(
+						"[<appl>,<term>]",
+						prefix
+							+ "pattern"
+							+ id
+							+ StringConversions.makeCapitalizedIdentifier(
+								alt.getId()),
+						alt.buildMatchPattern());
+				term_list = factory.makeList(entry, term_list);
+			}
+		}
+
+		return factory.make(
+			"[afuns(<term>),terms(<term>)]",
+			afun_list,
+			term_list);
+	}
+}
--- apigen/apigen/gen/c/Main.java
+++ apigen/apigen/gen/c/Main.java
@@ -1,25 +1,18 @@
 package apigen.gen.c;
 
 import java.io.*;
-import java.util.*;
-
-import aterm.*;
 import aterm.pure.PureFactory;
 
 import apigen.adt.*;
 import apigen.gen.TomSignatureGenerator;
 
 public class Main {
-	
-	private static boolean verbose;
-
-	private static boolean jtom;
-
-	private ATermFactory factory;
-
-	private InputStream input;
-
-	//{{{ private static void usage()
+	private static boolean verbose = false;
+	private static boolean jtom = false;
+	private static boolean make_term_compatibility = false;
+	private static String output = null;
+	private static String prologue = null;
+	private static String prefix = "";
 
 	private static void usage() {
 		System.err.println("usage: CGen [options]");
@@ -28,20 +21,14 @@ public class Main {
 		System.err.println("\t-input <in>               [-]");
 		System.err.println("\t-output <out>");
 		System.err.println("\t-prologue <prologue>");
+		System.err.println("\t-jtom");
 		System.exit(1);
 	}
 
-	//}}}
-
-	//{{{ public final static void main(String[] args)
-
 	public final static void main(String[] args) throws IOException {
-		String prefix = "";
 		String input = "-";
-		String output = null;
-		String prologue = null;
+
 		InputStream inputStream;
-		boolean make_term_compatibility = false;
 
 		if (args.length == 0) {
 			usage();
@@ -50,29 +37,21 @@ public class Main {
 		for (int i = 0; i < args.length; i++) {
 			if ("-help".startsWith(args[i])) {
 				usage();
-			}
-			else if ("-verbose".startsWith(args[i])) {
+			} else if ("-verbose".startsWith(args[i])) {
 				verbose = true;
-			}
-			else if ("-prefix".startsWith(args[i])) {
+			} else if ("-prefix".startsWith(args[i])) {
 				prefix = args[++i];
-			}
-			else if ("-output".startsWith(args[i])) {
+			} else if ("-output".startsWith(args[i])) {
 				output = args[++i];
-			}
-			else if ("-prologue".startsWith(args[i])) {
+			} else if ("-prologue".startsWith(args[i])) {
 				prologue = args[++i];
-			}
-			else if ("-input".startsWith(args[i])) {
+			} else if ("-input".startsWith(args[i])) {
 				input = args[++i];
-			}
-			else if ("-compatible:term".equals(args[i])) {
+			} else if ("-compatible:term".equals(args[i])) {
 				make_term_compatibility = true;
-			}
-			else if ("-jtom".startsWith(args[i])) {
-			   jtom = true;
-			}
-			else {
+			} else if ("-jtom".startsWith(args[i])) {
+				jtom = true;
+			} else {
 				usage();
 			}
 		}
@@ -82,95 +61,30 @@ public class Main {
 			if (output == null) {
 				usage();
 			}
-		}
-		else {
+		} else {
 			inputStream = new FileInputStream(input);
 			if (output == null) {
 				int extIndex = input.lastIndexOf((int) '.');
 				output = input.substring(0, extIndex);
 			}
-		}
 
-		Main gen =
-			new Main(inputStream, output, prefix, prologue, make_term_compatibility);
+			run(inputStream);
+		}
 	}
 
-	//}}}
+	static private void run(InputStream input) {
+		ADT adt;
 
-	//{{{ public CGen(InputStream input, String output, String prefix, String prologue)
+		try {
+			adt = new ADT(new PureFactory().readFromFile(input));
+			new APIGenerator(adt, output, prefix, verbose, true);
+			new TomSignatureGenerator(adt,new CTomSignatureImplementation(),".",output,verbose,true).run();
+			new CDictionaryGenerator(adt, ".", output, prefix, verbose, true).run();
 
-	public Main(InputStream input,String output,String prefix,String prologue,
-              boolean make_term_compatibility)
-		throws IOException {
-		this.input = input;
-		this.output = output;
-		this.capOutput = stringConverter.capitalize(output);
-		this.prefix = prefix;
-		this.prologue = prologue;
-		this.make_term_compatibility = make_term_compatibility;
-		afuns_by_name = new HashMap();
-		afuns_by_afun = new HashMap();
-
-		factory = new PureFactory();
-
-		ATerm adt = factory.readFromFile(input);
-
-		ADT api = new ADT(adt);
-
-		String header_name = output + ".h";
-		header = new PrintStream(new FileOutputStream(header_name));
-
-		String source_name = output + ".c";
-		source = new PrintStream(new FileOutputStream(source_name));
-
-        new APIGenerator(apiName,prefix,verbose,folding).run();
-        new TomSignatureGenerator(api,new CTomSignatureImplementation(),".",apiName,verbose,folding).run();
-        
-
-    info("generating " + output + ".dict");
-		ATerm dict = buildDictionary(api);
-		OutputStream dict_out = new FileOutputStream(output + ".dict");
-		dict.writeToTextFile(dict_out);
-	}
-
-	//}}}
-
-	//{{{ private ATerm buildDictionary(API api)
-
-	private ATerm buildDictionary(ADT api) {
-		ATermList afun_list = factory.makeList();
-		for (int i = nextAFun - 1; i >= 0; i--) {
-			String name = prefix + "afun" + i;
-			AFun afun = (AFun) afuns_by_name.get(name);
-			ATerm[] args = new ATerm[afun.getArity()];
-			for (int j = 0; j < afun.getArity(); j++) {
-				args[j] = factory.parse("x");
-			}
-			ATerm term = factory.makeAppl(afun, args);
-			afun_list = afun_list.insert(factory.make("[" + name + ",<term>]", term));
+		} catch (IOException e) {
+			System.out.println("Failed to read ADT from file");
+			System.exit(1);
 		}
 
-		ATermList term_list = factory.makeList();
-
-		Iterator types = api.typeIterator();
-		while (types.hasNext()) {
-			Type type = (Type) types.next();
-			String id = stringConverter.makeIdentifier(type.getId());
-			Iterator alts = type.alternativeIterator();
-			while (alts.hasNext()) {
-				Alternative alt = (Alternative) alts.next();
-				ATerm entry =
-					factory.make(
-						"[<appl>,<term>]",
-						prefix + "pattern" + id + stringConverter.makeCapitalizedIdentifier(alt.getId()),
-						alt.buildMatchPattern());
-				term_list = factory.makeList(entry, term_list);
-			}
-		}
-
-		return factory.make("[afuns(<term>),terms(<term>)]", afun_list, term_list);
 	}
-
-	//}}}
-	
 }
