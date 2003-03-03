209fcc4e16010ac4533e574e5325a6721a474a81
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-03-03 09:57:14 +0000

    *** empty log message ***

--- apigen/apigen/gen/java/JavaTypeConverter.java
+++
@@ -1,9 +0,0 @@
-package apigen.gen.java;
-
-import apigen.gen.TypeConverter;
-
-public class JavaTypeConverter extends TypeConverter {
-	public JavaTypeConverter() {
-		super("Integer","Double","String","ATerm");
-	}
-}
--- apigen/src/apigen/gen/c/CTypeConverter.java
+++
@@ -1,9 +0,0 @@
-package apigen.gen.c;
-
-import apigen.gen.TypeConverter;
-
-public class CTypeConverter extends TypeConverter {
-	public CTypeConverter() {
-		super("int","double","char*","ATerm");
-	}
-}
