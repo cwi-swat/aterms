7d89b7a762d1d40d1b5d69b2b914bfd80e6198e5
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2003-08-11 11:49:16 +0000

    added more constructors to the builtins.adt

--- apigen/test/builtinstest.c
+++ apigen/test/builtinstest.c
@@ -18,19 +18,19 @@ int main(int argc, char *argv[])
    * to be fixed!
    */
 
-  data[0] = (ATerm) makeIInteger(1);
+  data[0] = (ATerm) makeDInteger(1);
   assert(data[0] && ATisEqual(data[0], ATparse("int(1)")));
 
   data[1] = (ATerm) makeDDouble(1.0);
   assert(data[1] && ATisEqual(data[1], ATparse("double(1.0)")));
 
-  data[2] = (ATerm) makeSString("one");
+  data[2] = (ATerm) makeDString("one");
   assert(data[2] && ATisEqual(data[2], ATparse("str(\"one\")")));
 
-  data[3] = (ATerm) makeTTrm(ATparse("one"));
+  data[3] = (ATerm) makeDTrm(ATparse("one"));
   assert(data[3] && ATisEqual(data[3], ATparse("term(one)")));
 
-  data[4] = (ATerm) makeLLst((ATermList) ATparse("[one]"));
+  data[4] = (ATerm) makeDLst((ATermList) ATparse("[one]"));
   assert(data[4] && ATisEqual(data[4], ATparse("list([one])")));
 
 
