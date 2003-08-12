a077ff432d167dcf8390a081c961db605433bac5
Hayco de Jong <jong@metamud.org>
2003-08-12 07:24:05 +0000

    2003/08/12 <{jong,kooiker}@cwi.nl>  * aterm/Visitor.java: scrapped folding

--- aterm-java/aterm/Visitor.java
+++ aterm-java/aterm/Visitor.java
@@ -1,77 +1,47 @@
 package aterm;
 
-import jjtraveler.*;
+import jjtraveler.VisitFailure;
+import jjtraveler.VoidVisitor;
 
-public class Visitor extends VoidVisitor implements jjtraveler.Visitor
-{
-    public void voidVisit(jjtraveler.Visitable any)  throws VisitFailure {
-        if (any instanceof Visitable) {
-            ((Visitable) any).accept(this);
-        }
-        else {
-            throw new VisitFailure();
-        }
-    }
-    
-  //{{{ public void visitATerm(ATerm arg) throws ATermVisitFailure
+public class Visitor extends VoidVisitor implements jjtraveler.Visitor {
+	public void voidVisit(jjtraveler.Visitable any) throws VisitFailure {
+		if (any instanceof Visitable) {
+			((Visitable) any).accept(this);
+		}
+		else {
+			throw new VisitFailure();
+		}
+	}
 
-  public void visitATerm(ATerm arg) throws VisitFailure
-  {
-  }
+	public void visitATerm(ATerm arg) throws VisitFailure {
+		// do nothing
+	}
 
-  //}}}
+	public void visitInt(ATermInt arg) throws VisitFailure {
+		visitATerm(arg);
+	}
 
-  //{{{ public void visitInt(ATermInt arg) throws ATermVisitFailure
+	public void visitReal(ATermReal arg) throws VisitFailure {
+		visitATerm(arg);
+	}
 
-  public void visitInt(ATermInt arg) throws VisitFailure
-  {
-    visitATerm(arg);
-  }
+	public void visitAppl(ATermAppl arg) throws VisitFailure {
+		visitATerm(arg);
+	}
 
-  //}}}
-  //{{{ public void visitReal(ATermReal arg) throws ATermVisitFailure
+	public void visitList(ATermList arg) throws VisitFailure {
+		visitATerm(arg);
+	}
 
-  public void visitReal(ATermReal arg) throws VisitFailure
-  {
-    visitATerm(arg);
-  }
+	public void visitPlaceholder(ATermPlaceholder arg) throws VisitFailure {
+		visitATerm(arg);
+	}
 
-  //}}}
-  //{{{ public void visitAppl(ATermAppl arg) throws ATermVisitFailure
+	public void visitBlob(ATermBlob arg) throws VisitFailure {
+		visitATerm(arg);
+	}
 
-  public void visitAppl(ATermAppl arg) throws VisitFailure
-  {
-    visitATerm(arg);
-  }
-
-  //}}}
-  //{{{ public void visitList(ATermList arg) throws ATermVisitFailure
-
-  public void visitList(ATermList arg) throws VisitFailure
-  {
-    visitATerm(arg);
-  }
-
-  //}}}
-  //{{{ public void visitPlaceholder(ATermPlaceholder arg) throws ATermVisitFailure
-
-  public void visitPlaceholder(ATermPlaceholder arg) throws VisitFailure
-  {
-    visitATerm(arg);
-  }
-
-  //}}}
-  //{{{ public void visitBlob(ATermBlob arg) throws ATermVisitFailure
-
-  public void visitBlob(ATermBlob arg) throws VisitFailure
-  {
-    visitATerm(arg);
-  }
-
-  //}}}
-  
-  public void visitAFun(AFun fun) throws VisitFailure 
-  {
-      // do nothing
-  }
+	public void visitAFun(AFun fun) throws VisitFailure {
+		// do nothing
+	}
 }
