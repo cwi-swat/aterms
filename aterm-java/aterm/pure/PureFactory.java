691685fbf2dcc434b451062b24bb0ca41ad7e56f
Hayco de Jong <jong@metamud.org>
2003-09-01 11:49:36 +0000

    * reverted public access to private member "empty"   use public method getEmpty() instead!
      thou shalt not make members public, when you need access, create
      public accessors instead!

--- aterm-java/aterm/pure/PureFactory.java
+++ aterm-java/aterm/pure/PureFactory.java
@@ -56,7 +56,7 @@ public class PureFactory extends SharedObjectFactory implements ATermFactory {
 	private ATermPlaceholderImpl protoPlaceholder;
 	private AFunImpl protoAFun;
 
-	public ATermList empty;
+	private ATermList empty;
 
 	static boolean isBase64(int c) {
 		return Character.isLetterOrDigit((char) c) || c == '+' || c == '/';
