4bfb0bd9160fd89d89f5ba28020326326d1be070
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2004-09-22 10:29:05 +0000

    * Fixed some distcheck problems in apigen/adt/api  * Added cyclic.adt to EXTRA_DIST

--- apigen/test/cyclic.java.sh
+++ apigen/test/cyclic.java.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-CLASSPATH=.:..:/Volumes/Home/pem/CWI/share/aterm-1.6.jar:/Volumes/Home/pem/CWI/share/shared-objects-1.4.jar:/Volumes/Home/pem/CWI/share/jjtraveler-0.4.3.jar
+CLASSPATH=.:..:/ufs/jurgenv/glt/installed/1-5/share/aterm-1.6.jar:/ufs/jurgenv/glt/installed/1-5/share/shared-objects-1.4.jar:/ufs/jurgenv/glt/installed/1-5/share/jjtraveler-0.4.3.jar
 export CLASSPATH
 java test.CyclicTest ../test
