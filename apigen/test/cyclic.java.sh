06f4b0c117eff8a3a51d52256f614d47f7ebaa0f
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2004-09-22 10:35:15 +0000

    * removed generated test script from repository

--- apigen/test/cyclic.java.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-CLASSPATH=.:..:/ufs/jurgenv/glt/installed/1-5/share/aterm-1.6.jar:/ufs/jurgenv/glt/installed/1-5/share/shared-objects-1.4.jar:/ufs/jurgenv/glt/installed/1-5/share/jjtraveler-0.4.3.jar
-export CLASSPATH
-java test.CyclicTest ../test
