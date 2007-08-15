7b58766760ac826d46a48c372e1143011d452f4b
Jurgen J. Vinju <Jurgen.Vinju@cwi.nl>
2007-08-15 06:39:29 +0000

    * aterm/aterm1.h: moved struct definitions to aterm/encoding.h such that    aterm1.h contains only API stuff and not hidden implementation details.  * aterm/aterm1.h: doxygen'd aterm1.h

--- aterm/aterm/encoding.h
+++ aterm/aterm/encoding.h
@@ -4,6 +4,7 @@
 
 #ifndef ENCODING_H
 #define ENCODING_H
+#include "atypes.h"
 
 #ifdef __cplusplus
 extern "C"
@@ -129,6 +130,28 @@ typedef unsigned int header_type;
 
 #define AT_TABLE_SIZE(table_class)  (1<<(table_class))
 #define AT_TABLE_MASK(table_class)  (AT_TABLE_SIZE(table_class)-1)
+
+#define MAX_ARITY             MAX_LENGTH
+
+#define MIN_TERM_SIZE         TERM_SIZE_APPL(0)
+#define INITIAL_MAX_TERM_SIZE 256
+
+
+struct __ATerm
+{
+  header_type   header;
+  union _ATerm *next;
+};
+
+typedef union _ATerm
+{
+  header_type     header;
+  struct __ATerm  aterm;
+  union _ATerm*   subaterm[1];
+  MachineWord     word[1];
+} *ATerm;
+
+typedef void (*ATermProtFunc)();
 
 #ifdef __cplusplus
 }
