9e2030b1456929910c916e6bff6a1929330a57f2
Pierre-Etienne Moreau <pemoreau@gmail.com>
2007-11-04 14:59:14 +0000

    * very small improve of the get method (inline of findObject,     discriminate on e.hash first, try to avoid the findObjectWhileUnderLock)   * re-indent the copyright and apply Java Sun's style

--- shared-objects/src/shared/SingletonSharedObjectFactory.java
+++ shared-objects/src/shared/SingletonSharedObjectFactory.java
@@ -1,32 +1,42 @@
 /*
- * Copyright (c) 2003-2007, CWI and INRIA All rights reserved. Redistribution and use in source and
- * binary forms, with or without modification, are permitted provided that the following conditions
- * are met: * Redistributions of source code must retain the above copyright notice, this list of
- * conditions and the following disclaimer. * Redistributions in binary form must reproduce the
- * above copyright notice, this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution. * Neither the name of the University of
- * California, Berkeley nor the names of its contributors may be used to endorse or promote products
- * derived from this software without specific prior written permission. THIS SOFTWARE IS PROVIDED
- * BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
- * NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2003-2007, CWI and INRIA
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met: 
+ * 	- Redistributions of source code must retain the above copyright
+ * 	notice, this list of conditions and the following disclaimer.  
+ * 	- Redistributions in binary form must reproduce the above copyright
+ * 	notice, this list of conditions and the following disclaimer in the
+ * 	documentation and/or other materials provided with the distribution.
+ * 	- Neither the name of the CWI, INRIA nor the names of its
+ * 	contributors may be used to endorse or promote products derived from
+ * 	this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package shared;
 
 import shared.SharedObjectFactory;
 
-public class SingletonSharedObjectFactory{
+public class SingletonSharedObjectFactory {
 	
 	/**
 	 * Default constructor. Made private to prevent instantiation from outside this class.
 	 */
-	private SingletonSharedObjectFactory(){
+	private SingletonSharedObjectFactory() {
 		super();
 	}
 	
@@ -35,7 +45,7 @@ public class SingletonSharedObjectFactory{
 	 * 
 	 * @return A reference to the shared object factory.
 	 */
-	public static SharedObjectFactory getInstance(){
+	public static SharedObjectFactory getInstance() {
 		return InstanceKeeper.instance;
 	}
 	
@@ -44,7 +54,7 @@ public class SingletonSharedObjectFactory{
 	 * 
 	 * @author Arnold Lankamp
 	 */
-	private static class InstanceKeeper{
+	private static class InstanceKeeper {
 		public final static SharedObjectFactory instance = new SharedObjectFactory();
 	}
 }
