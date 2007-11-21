c42847276541d7d8b897d0fbe97f5cf747ea7b8e
Taeke Kooiker <taeke@kooiker.info>
2007-11-21 09:24:02 +0000

    * aterm/aterm.c: set standard streams to binary mode for WIN32

--- aterm/aterm/aterm.c
+++ aterm/aterm/aterm.c
@@ -6,6 +6,10 @@
 #include <string.h>
 #include <assert.h>
 
+#ifdef WIN32
+#include <fcntl.h>
+#endif
+
 #include "_aterm.h"
 #include "memory.h"
 #include "afun.h"
@@ -242,6 +246,18 @@ ATinit(int argc, char *argv[], ATerm * bottomOfStack)
   AT_initBafIO(argc, argv);
 
   /*}}}  */
+
+#ifdef WIN32
+  if (_setmode(_fileno(stdin), _O_BINARY) == -1) {
+    perror("Warning: Cannot set inputstream to binary mode.");
+  }
+  if (_setmode(_fileno(stdout), _O_BINARY) == -1) {
+    perror("Warning: Cannot set outputstream to binary mode.");
+  }
+  if (_setmode(_fileno(stderr), _O_BINARY) == -1) {
+    perror("Warning: Cannot set errorstream to binary mode.");
+  }
+#endif 
 
   initialized = ATtrue;
 
